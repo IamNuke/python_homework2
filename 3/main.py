8334ce4889c9035c9f8ecaa91d5eda58358878eb
IamNuke <alexeimartynyuk@gmail.com>
2023-02-03 11:25:49 +0300

    Third assignment

---
+++ 3/main.py
@@ -0,0 +1,9 @@
+import math
+n = int(input("Enter number:"))
+
+i = 0
+result = int(math.pow(2, i))
+while result <= n:
+    print(f'{result} ', end='')
+    i += 1
+    result = int(math.pow(2, i))
