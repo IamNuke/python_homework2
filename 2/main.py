c3437ae513278815fed9f3a3d8deedbc62c7ce83
IamNuke <alexeimartynyuk@gmail.com>
2023-02-03 11:14:53 +0300

    Second assignment. Fix

--- 2/main.py
+++ 2/main.py
@@ -3,7 +3,7 @@
 numbers_sum = int(input('Enter summ of numbers :'))
 numbers_product = int(input('Enter product of numbers :'))
 
-for x in range(1000):
+for x in range(1001):
     if (numbers_sum - x)*x == numbers_product:
         print(f'Numbers: {x}; {numbers_sum - x}')
         break
